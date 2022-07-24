a6840a99d26a65082dccfa8a08af7862350df181
meqdad <meqdad.moradi@outlook.com>
2022-07-24 20:56:12 +0200

    dropdown menu added and designed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./../assets/images/logo.svg";
+import todoListIcon from "./../assets/images/icon-todo.svg";
+import calenderIcon from "./../assets/images/icon-calendar.svg";
+import reminderIcon from "./../assets/images/icon-reminders.svg";
+import planningIcon from "./../assets/images/icon-planning.svg";
+
+import arrowDown from "../assets/images/icon-arrow-down.svg";
+import arrowUp from "../assets/images/icon-arrow-up.svg";
 
 const Header = () => {
+   const [featuresMenu, setFeaturesMenu] = useState(false);
+   const [companyMenu, setCompanyMenu] = useState(false);
+
+   const features = () => {
+      setFeaturesMenu(!featuresMenu);
+      setCompanyMenu(false);
+   };
+
+   const company = () => {
+      setCompanyMenu(!companyMenu);
+      setFeaturesMenu(false);
+   };
+
    return (
       <header className="header">
          <div className="container">
             {/* logo */}
             <div className="logo">
-               <a href="#">
+               <a href="#s">
                   <img src={logo} alt="snap" />
                </a>
             </div>
@@ -15,17 +35,82 @@ const Header = () => {
             {/* primary navigation */}
             <nav className="primary-nav">
                <ul className="nav-list">
-                  <li>
-                     <a href="#">features</a>
+                  <li
+                     className={
+                        featuresMenu
+                           ? "primary-nav-link active"
+                           : "primary-nav-link"
+                     }
+                     onClick={() => features()}
+                  >
+                     <a href="#s">
+                        features{" "}
+                        <img src={featuresMenu ? arrowUp : arrowDown} alt="" />
+                     </a>
+
+                     {/* sub menu */}
+                     <div className="sub-menu">
+                        <ul className="sub-menu-list">
+                           <li>
+                              <a href="https://meqdad-moradi.github.io/todo-list/" target="blank">
+                                 <img src={todoListIcon} alt="" />
+                                 todo list
+                              </a>
+                           </li>
+                           <li>
+                              <a href="#s">
+                                 <img src={calenderIcon} alt="" />
+                                 calender
+                              </a>
+                           </li>
+                           <li>
+                              <a href="#s">
+                                 <img src={reminderIcon} alt="" />
+                                 reminders
+                              </a>
+                           </li>
+                           <li>
+                              <a href="#s">
+                                 <img src={planningIcon} alt="" />
+                                 planning
+                              </a>
+                           </li>
+                        </ul>
+                     </div>
                   </li>
-                  <li>
-                     <a href="#">companys</a>
+                  <li
+                     className={
+                        companyMenu
+                           ? "primary-nav-link active"
+                           : "primary-nav-link"
+                     }
+                     onClick={() => company()}
+                  >
+                     <a href="#s">
+                        companys{" "}
+                        <img src={companyMenu ? arrowUp : arrowDown} alt="" />
+                     </a>
+
+                     {/* sub menu */}
+                     <div className="sub-menu">
+                        <ul className="sub-menu-list">
+                           <li>
+                              <a href="#s">history</a>
+                           </li>
+                           <li>
+                              <a href="#s">Our Team</a>
+                           </li>
+                           <li>
+                              <a href="#s">Blog</a>
+                           </li>
+                        </ul>
+                     </div>
                   </li>
-                  <li>
-                     <a href="#">careers</a>
+                  <li className="primary-nav-link">
+                     <a href="#s">careers</a>
                   </li>
-                  <li>
-                     <a href="#">about</a>
+                  <li className="primary-nav-link">
+                     <a href="#s">about</a>
                   </li>
                </ul>
             </nav>
